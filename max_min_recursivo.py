b045f1911fa8a1ee6df377abee1c055b2586f4d6
jasiel Serra <jasiel_serra@hotmail.com>
2022-01-23 23:08:44 -0300

    Calculando o maximo e o minimo utilizando uma função recursiva.

---
+++ max_min_recursivo.py
@@ -0,0 +1,34 @@
+from random import shuffle
+
+def max_min(lst):
+    ''' Calculate the maximun and minimum of a list lst
+     :param lst: list
+     :return: tuple
+     '''
+    n = len(lst)
+    if n == 0: # lst = [1, ... 99] #O(n)
+        raise ValueError('Empty List')    #O(n)
+    max_value = min_value = lst[-1]       #O(n)
+
+    def max_min_rec(cursor):
+        if cursor < 0:
+            return max_value, min_value
+        current = lst[cursor]
+        if current > max_value:
+            max_value = current
+        if current < min_value:
+            min_value = current
+        return (cursor - 1)
+
+    return max_min_rec(n-1)
+
+
+
+
+print(max_min([1])) # 1,1
+print(max_min([1,2])) # 2,1
+random_list = list(range(100))
+shuffle(random_list)
+print(random_list)
+print(max_min(list(range(100)))) # 99,0
+print(max_min([]))
