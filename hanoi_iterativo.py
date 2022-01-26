9382497bb278a1649444b34f7ab286f138a1d2b5
jasiel Serra <jasiel_serra@hotmail.com>
2022-01-25 23:37:16 -0300

    Iterative Solution to Hanoi Tower.

---
+++ hanoi_iterativo.py
@@ -0,0 +1,32 @@
+def hanoi(discos, orig='A', aux='B', dest='C'):
+    """Recursive Solution to Hanoi Tower
+      t(n) = 1 + 2(t(n-1))
+      t(n) = 1 + 2 + 4(t(n-2))
+      t(n) = 1 + 2 + 4 + 8(t(n-3))
+      t(n) = 1 + 2 + 4 + 8 + 16(t(n-4))
+      t(n) = 1 + 2 + 4 + 8 + 16 ... 2**n
+      2(t(n)) = 2 + 4 + 8 + 16 + 32 ... 2**(n+1)
+      t(n)=2**(n+1)-1 => O(2**n)
+      m(n) = 1 + m(n-1) => O(n)
+      """
+    stack = [(False,discos, orig, aux, dest)]
+
+    while stack:
+        print_flag, discos, orig, aux, dest =  stack.pop()
+        if discos < 1:
+            continue
+        if not print_flag:
+            stack.append((True, discos, orig, aux, dest))
+            stack.append((False, discos - 1, orig, dest, aux))
+        else:
+            print(f'{orig} -> {dest}: {discos}')
+            stack.append((False, discos - 1, aux, orig, dest))
+
+    #if discos >= 1:
+    #    hanoi(discos-1, orig, dest, aux)
+    #    print(f'{orig} -> {dest} : {discos}')
+    #   hanoi(discos-1, aux, orig, dest)
+
+for i in range(1,4):
+    print('####### Hanoi %s' %i)
+    hanoi(i)
--- hanoi_rec.py
+++ hanoi_rec.py
@@ -1,10 +1,19 @@
-def hanoi(n, orig='A', aux='B', dest='C'):
-    '''Recursive Solution to Hanoi Tower'''
+def hanoi(discos, orig='A', aux='B', dest='C'):
+    """Recursive Solution to Hanoi Tower
+      t(n) = 1 + 2(t(n-1))
+      t(n) = 1 + 2 + 4(t(n-2))
+      t(n) = 1 + 2 + 4 + 8(t(n-3))
+      t(n) = 1 + 2 + 4 + 8 + 16(t(n-4))
+      t(n) = 1 + 2 + 4 + 8 + 16 ... 2**n
+      2(t(n)) = 2 + 4 + 8 + 16 + 32 ... 2**(n+1)
+      t(n)=2**(n+1)-1 => O(2**n)
+      m(n) = 1 + m(n-1) => O(n)
+      """
 
-    if n >= 1:
-        hanoi(n-1, orig, dest, aux)
-        print('{} -> {} : {}'.format(orig, dest, n))
-        hanoi(n-1, aux, orig, dest)
+    if discos >= 1:
+        hanoi(discos-1, orig, dest, aux)
+        print(f'{orig} -> {dest} : {discos}')
+        hanoi(discos-1, aux, orig, dest)
 
 for i in range(1,4):
     print('####### Hanoi %s' %i)
