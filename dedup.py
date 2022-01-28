a86ef13a992451e691d9a02bc77d16b4a2c51e78
jasiel Serra <jasiel_serra@hotmail.com>
2022-01-28 00:38:21 -0300

    Informada a complexidade deste algoritmo.

--- dedup.py
+++ dedup.py
@@ -3,12 +3,16 @@ def dedup(lst):
     Remove duplicates from lst
     :param lst: a list
     :return: new list without duplicated elements
+
+    linear for time and space
     '''
     result = []
+    repeated = set()
 
     for e in lst:
-        if e not in result:
+        if e not in repeated:
             result.append(e)
+            repeated.add(e)
 
     return result
 
