d946125f06969b500cd2dbef559a20f184cd5f71
jasiel Serra <jasiel_serra@hotmail.com>
2022-02-05 21:54:28 -0300

    Utilizando funções recursivas

--- phoneword.py
+++ phoneword.py
@@ -9,5 +9,19 @@ def phoneword(phonenumber):
         '6': 'mno', '7':'pqrs', '8': 'tuv', '9': 'wxyz',
     }
 
+    n = len(phonenumber)
+
+    def phoneword_rec(previous_results, cursor):
+        if cursor == n:
+            return previous_results
+        digit = phonenumber[cursor]
+        results = []
+        for char in digit_to_chars[digit]:
+            results.extend(
+                prev_result + char for prev_result in previous_results)
+        return phoneword_rec(results, cursor + 1)
+
+    return phoneword_rec([''], 0)
+
 print(phoneword('')) # ['']
 print(phoneword('736')) #['REN' 'SEN']
